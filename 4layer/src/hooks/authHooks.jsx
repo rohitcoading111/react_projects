a0856225e58a8241743f8a891e7814aa54a8a90b
rohitcoading111 <rp538220@gmail.com>
2026-08-07 18:19:49 +0530

    this is the login and updatin functanallity with the help of redux toolkit

--- 4layer/src/hooks/authHooks.jsx
+++ 4layer/src/hooks/authHooks.jsx
@@ -1,16 +1,41 @@
 import { useNavigate } from "react-router-dom";
-import  {useForm} from 'react-hook-form'
+import  {set, useForm} from 'react-hook-form'
+import { useState } from "react";
+import { useDispatch } from 'react-redux';
+import { toast } from "react-toastify";
+import { addUser } from "../features/authSlice";
 
 export const useAuth = () =>{
     let navigate = useNavigate();
+    let dispatch = useDispatch();
+
+    const [registeredUsers, setregisteredUsers] = useState(JSON.parse(localStorage.getItem("registeredUsers"))||[])
     
     let {register,handleSubmit,reset,formState:{errors}} = useForm();
 
     const registerForm = (data)=> {
+     let arr = [...registeredUsers,data]
+      setregisteredUsers(arr)
+      localStorage.setItem("registeredUsers",JSON.stringify(arr));
+      toast.success("user registered successfully")
+      reset();
+    } 
+
+    const loginForm = (data)=> {
+        let user =registeredUsers.find((val)=>{
+            return val.email === data.email && val.password === data.password;
+        })
+        if(!user){
+         toast.error("invalid login")
+         return
+        }
+         
+         dispatch(addUser(user));
+         toast.success("user logged in")
+         reset();
+        
 
     }
 
-    const loginForm = (data)=> {}
-    
     return {navigate, register, handleSubmit, reset, errors, registerForm, loginForm}
-}
+} 
--- 4layer/src/main.jsx
+++ 4layer/src/main.jsx
@@ -4,10 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import { store } from './app/store.jsx'
 import AppRoutes from './routes/AppRoutes.jsx'
-
+import { ToastContainer} from 'react-toastify';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <AppRoutes />
+    <ToastContainer/>
   </Provider>
 )
